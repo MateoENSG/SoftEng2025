533af3f0a4d4b64da66970eacfbdb23573addc1d
MateoENSG <mateo.btmpsrodrigues@gmail.com>
2025-09-18 12:32:27 +0200

    Tidying the project folder, this needed the creation of a static library and folders (including CMakeLists.txt files for some folders).

---
+++ include/basic/message.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <HelloProjectConfig.h>
+
+//print of the word "Hello"
+void hello_world();
+
+//print of the version
+void print_version();
--- src/basic/message.cpp
+++ src/basic/message.cpp
@@ -1,21 +1,17 @@
 #include <iostream>
 #include <HelloProjectConfig.h>
+#include <basic/message.h>
 
+//print of the word "Hello"
 void hello_world(){
 	std::cout<<"Hello World!\n";
 }
 
+//print of the version
 void print_version() {
 	std::cout << "project version is "
 		<< HelloProject_VERSION_MAJOR
 		<< "."
 		<< HelloProject_VERSION_MINOR
 		<< std::endl;
-}
-
-int main()
-{
-	hello_world();
-	print_version();
-	return 0;
 }
