673cb2ecc4a59b4563f16688b89e0073913e149e
MateoENSG <mateo.btmpsrodrigues@gmail.com>
2025-09-18 11:16:22 +0200

    Add of version printing in hello.cpp and CMakeLists.txt

--- hello.cpp
+++ hello.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
+#include <HelloProjectConfig.h>
 
 void hello_world(){
 	std::cout<<"Hello World!\n";
 }
 
+void print_version() {
+	std::cout << "project version is "
+		<< HelloProject_VERSION_MAJOR
+		<< "."
+		<< HelloProject_VERSION_MINOR
+		<< std::endl;
+}
 
 int main()
 {
 	hello_world();
+	print_version();
 	return 0;
 }
